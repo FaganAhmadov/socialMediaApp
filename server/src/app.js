e808ee0fcaa5d2211992b61963e1d9ebc0bbdf49
FaganAhmadov <faganahmadov933@gmail.com>
2026-08-19 17:10:35 +0400

    refactor:vercel json

---
+++ server/src/app.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv')
+const express = require("express");
+const router = require("./routers");
+const { mongoDbConnection } = require("./db");
+const config = require("./config");
+const cookieParser = require('cookie-parser');
+const sendMail = require('./utils/sendMail');
+const corsMiddleware = require('./middleware/corsMiddleware');
+const app = express();
+const path = require('path');
+
+
+app.use(corsMiddleware);
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// file reqden almaq ucun
+app.use("/api/v1", router)
+app.use('/uploads', express.static(path.join(__dirname, './uploads')));
+mongoDbConnection()
+
+
+
+module.exports = app;
